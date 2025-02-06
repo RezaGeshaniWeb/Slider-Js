0f14cdc000b2da1e74d50fd1c744b3fbd01e2f58
Reza Geshani <rezageshaniweb@gmail.com>
2025-02-06 15:51:32 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function updateContent() {
     mainContent.classList.add('animate-anime')
 }
 
-setInterval(nextImg, 4000)
+setInterval(nextImg, 5000)
 nextBtn.addEventListener('click', nextImg)
 
 function nextImg() {
